8617833d1023535e187af9a8197c69f90d26b219
Harjuno Perwironegoro <hp1515@matrix34.doc.ic.ac.uk>
2016-01-13 21:16:53 +0000

    Heuristics now have an associated name, can be selected from that name and can also be listed

--- uno/perwironegoro/boardgames/pawnrace/ai/Heuristic.java
+++ uno/perwironegoro/boardgames/pawnrace/ai/Heuristic.java
@@ -2,19 +2,25 @@ package uno.perwironegoro.boardgames.pawnrace.ai;
 
 public class Heuristic {
 
-	protected int
+	public static final Heuristic
+	Max = new Heuristic(10, 4, 3, 2, 10, 0, "Max"),
+	Edgar = new Heuristic(10, 4, 3, 2, 10, 0, "Edgar");
+	
+	protected final int
 	betweenEdge,
 	pastEdge,
 	onEdge,
 	passed,
 	pawnExists,
 	protectScore;
+	
+	protected final String name;
 
 	/**
 	 * Multipliers: passed . , pastEdge .'' , onEdge :' , betweenEdge '.',
 	 * pawn exists, protecting score
 	 */
-	public Heuristic(int passed, int pastEdge, int onEdge, int betweenEdge, int pawnExists, int protectScore) {
+	public Heuristic(int passed, int pastEdge, int onEdge, int betweenEdge, int pawnExists, int protectScore, String name) {
 		this.passed = passed;
 		this.pastEdge = pastEdge;
 		this.onEdge = onEdge;
@@ -22,5 +28,27 @@ public class Heuristic {
 		
 		this.pawnExists = pawnExists;
 		this.protectScore = protectScore;
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public static Heuristic getHeuristic(String name) {
+		for(Heuristic h : Heuristic.getAllHeuristics()) {
+			if(name.equals(h.getName().toLowerCase())) {
+				return h;
+			}
+		}
+		return null;
+	}
+	
+	public static Heuristic[] getAllHeuristics() {
+		Heuristic[] hs = new Heuristic[2];
+		hs[0] = Max;
+		hs[1] = Edgar;
+		return hs;
 	}
 }
