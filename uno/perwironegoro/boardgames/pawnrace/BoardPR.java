50f60dc21421eaa4cc98cf4a5024601af6ad3a1e
Uno <hperwironegoro@gmail.com>
2016-01-16 18:34:35 +0000

    Added a special character to denote a board setup with full pawns

--- uno/perwironegoro/boardgames/pawnrace/BoardPR.java
+++ uno/perwironegoro/boardgames/pawnrace/BoardPR.java
@@ -5,10 +5,12 @@ import uno.perwironegoro.boardgames.Colour;
 import uno.perwironegoro.boardgames.UtilsBoard;
 
 public class BoardPR extends Board {
+	public static final char ARBCHAR = '@';
+
 	public BoardPR(BoardPR b) {
 		super(b.getWidth(), b.getHeight());
-		for(int x = 1; x <= b.getWidth(); x++) {
-			for(int y = 1; y <= b.getHeight(); y++) {
+		for (int x = 1; x <= b.getWidth(); x++) {
+			for (int y = 1; y <= b.getHeight(); y++) {
 				copyOntoBoard(b.getSquare(x, y));
 			}
 		}
@@ -20,22 +22,25 @@ public class BoardPR extends Board {
 
 	public BoardPR(int width, int height, char whiteGap, char blackGap) {
 		super(width, height);
-		assert(width >= 2): "Min board width: 2";
-		assert(height >= 4): "Max board height: 4";
-		for(int x = 0; x < width; x++) {
+		assert (width >= 2) : "Min board width: 2";
+		assert (height >= 4) : "Max board height: 4";
+		for (int x = 0; x < width; x++) {
 			squares[x][height - 2].setOccupier(Colour.BLACK);
 			squares[x][1].setOccupier(Colour.WHITE);
 		}
-		squares[UtilsBoard.alphaCharToIndex(blackGap) - 1][height - 2].setOccupier(Colour.NONE);
-		squares[UtilsBoard.alphaCharToIndex(whiteGap) - 1][1].setOccupier(Colour.NONE);
+		if (blackGap != ARBCHAR) {
+			squares[UtilsBoard.alphaCharToIndex(blackGap) - 1][height - 2].setOccupier(Colour.NONE);
+		}
+		if (whiteGap != ARBCHAR) {
+			squares[UtilsBoard.alphaCharToIndex(whiteGap) - 1][1].setOccupier(Colour.NONE);
+		}
 	}
 
 	public void applyMove(MovePR move) {
 		super.applyMove(move);
 
-		if(move.isEnPassant()) {
-			UtilsPR.getBoardSquareBefore(move.getTo(), this)
-			.setOccupier(Colour.NONE);
+		if (move.isEnPassant()) {
+			UtilsPR.getBoardSquareBefore(move.getTo(), this).setOccupier(Colour.NONE);
 		}
 	}
 
@@ -44,10 +49,9 @@ public class BoardPR extends Board {
 
 		Colour movedPlayer = move.getTo().getOccupier();
 
-		if(move.isEnPassant()) {
-			getBoardSquare(UtilsPR.getBoardSquareBefore(move.getTo(), this))
-			.setOccupier(movedPlayer.opposite());
-		} else if(move.isCapture()) {
+		if (move.isEnPassant()) {
+			getBoardSquare(UtilsPR.getBoardSquareBefore(move.getTo(), this)).setOccupier(movedPlayer.opposite());
+		} else if (move.isCapture()) {
 			getBoardSquare(move.getTo()).setOccupier(movedPlayer.opposite());
 		}
 	}
